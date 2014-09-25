5550d31a889d3febfc6c1b619d0fd75afa67a95b
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2014-09-25 11:41:54 +0100

    Added a basic src/Makefile to support non-Eclipse builds

--- src/swmr-reader.cpp
+++ src/swmr-reader.cpp
@@ -1,7 +1,30 @@
-
 #include <iostream>
+#include <string>
+#include <assert.h>
+#include <sys/time.h>
+
+
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+#include <log4cxx/ndc.h>
+using namespace log4cxx;
+using namespace log4cxx::xml;
+using namespace log4cxx::helpers;
+
 
 #include "hdf5.h"
+#include "swmr-testdata.h"
 
 using namespace std;
+
+int main() {
+  DOMConfigurator::configure("Log4cxxConfig.xml");
+  LoggerPtr log(Logger::getLogger("main"));
+
+  LOG4CXX_DEBUG(log, "this is a debug message.");
+
+
+
+  return 0;
+}
 
